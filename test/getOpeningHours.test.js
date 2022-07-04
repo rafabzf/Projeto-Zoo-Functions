3f883023dec6ad5266af1ea5da88d335a863d437
Rafael <rafinhabzf@gmail.com>
2022-07-04 15:45:19 -0300

    Requisito 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -41,4 +41,33 @@ describe('Testes da função getOpeningHours', () => {
 
     expect(actual).toBe(expected);
   });
+
+  it('Caso não for passado nenhum parâmetro, retorna todos os horários dos dias', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('Verifica se o zoo está fechado', () => {
+    const actual = getOpeningHours('Monday', '3:00-AM');
+    const expected = 'The zoo is closed';
+
+    expect(actual).toBe(expected);
+  });
+
+  it('Verifica se o zoo está aberto', () => {
+    const actual = getOpeningHours('Friday', '10:00-AM');
+    const expected = 'The zoo is open';
+
+    expect(actual).toBe(expected);
+  });
 });
